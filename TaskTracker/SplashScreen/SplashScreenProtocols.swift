adb9691b9627a100d887bf462aecfb7f8f4d14c6
Andrey Tsurka <plant1k@yandex.ru>
2021-12-21 15:44:05 +0300

    add protocolsSplash

---
+++ TaskTracker/SplashScreen/SplashScreenProtocols.swift
@@ -0,0 +1,16 @@
+//
+//  SplashScreenProtocols.swift
+//  TaskTracker
+//
+//  Created by user on 21.12.2021.
+//
+
+import Foundation
+
+protocol SplashScreenPresenterProtocol {
+    
+}
+
+protocol SplashScreenViewProtocol {
+    
+}
